220bc89a5cfdbaf0d221e0e1d821d2f8df4d281c
SubodhJondhale <122435965+SubodhJondhale@users.noreply.github.com>
2025-08-25 19:56:57 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,8 @@
 import { io } from 'socket.io-client';
 
 // Replace with your actual backend URL
-//const socketUrl = 'http://127.0.0.1:5000';
- const socketUrl = 'http://34.47.214.154:5000';
+const socketUrl = 'https://127.0.0.1:5000';
+// const socketUrl = 'http://34.47.214.154:5000';
 const socket = io(socketUrl, {
     transports: ['websocket'],
     withCredentials: true,
