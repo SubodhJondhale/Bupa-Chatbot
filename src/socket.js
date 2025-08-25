3292fe6c2596f8b47ec6c88e05aaa438b6f203d5
SubodhJondhale <122435965+SubodhJondhale@users.noreply.github.com>
2025-08-25 20:57:36 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,18 @@ import { io } from 'socket.io-client';
 // Replace with your actual backend URL
 //const socketUrl = 'http://127.0.0.1:5000';
 //const socketUrl = 'http://0.0.0.0:5000';
- const socketUrl = 'https://subodh.live:5000';
+//  const socketUrl = 'https://subodh.live:5000';
+// const socket = io(socketUrl, {
+//     transports: ['websocket'],
+//     withCredentials: true,
+//     reconnection: true,
+//     reconnectionAttempts: 5,
+//     reconnectionDelay: 1000,
+//     reconnectionDelayMax: 5000,
+// });
+
+// export default socket;
+const socketUrl = process.env.REACT_APP_SOCKET_URL || 'http://127.0.0.1:5000';
 const socket = io(socketUrl, {
     transports: ['websocket'],
     withCredentials: true,
