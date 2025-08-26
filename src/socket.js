e4e899ae94f5dba96108a1b90bbf8c17de192c83
SubodhJondhale <122435965+SubodhJondhale@users.noreply.github.com>
2025-08-26 16:31:30 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -2,8 +2,8 @@ import { io } from 'socket.io-client';
 
 // Replace with your actual backend URL
 // const socketUrl = 'https://cool-lions-cut.loca.lt';
-//const socketUrl = 'https://34.47.214.154';
-  const socketUrl = 'https://subodh.live';
+const socketUrl = 'https://34.47.214.154';
+  //const socketUrl = 'https://subodh.live';
 const socket = io(socketUrl, {
     transports: ['websocket'],
     withCredentials: true,
